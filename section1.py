e2149802084fbac00e367c4668e2581a3d357da5
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-04-24 01:56:13 -0500

    work in progress

--- section1.py
+++ section1.py
@@ -2,3 +2,41 @@
 
 my_name = "Luis Contreras"
 print("Hello and welcome " + my_name + "!")
+print('Luis Contreras')
+
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "Lunch"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
+
+# You might encounter a SyntaxError if you open a string with double quotes and end it with a single quote. Update the string so that it starts and ends with the same punctuation.
+
+# You might encounter a NameError if you try to print a single word string but fail to put any quotes around it. Python expects the word of your string to be defined elsewhere but can’t find where it’s defined. Add quotes to either side of the string to squash this bug.
+
+# Update the malformed strings in the workspace to all be strings.
+
+print("This message has mismatched quote marks!")
+print("Abracadabra")
+
+# Define the release and runtime integer variables below:
+release_year = 2019
+runtime = 150
+
+
+# Define the rating_out_of_10 float variable below:
+rating_out_of_10 = 9.5
