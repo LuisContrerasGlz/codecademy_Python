6d5dd30ff004f0b92c31abe42447aeb600ebf325
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-04-25 23:44:10 -0500

    section 1 done

--- section1.py
+++ section1.py
@@ -177,3 +177,28 @@ customer_one_total += customer_one_tax
 
 print(customer_one_itemization)
 print(customer_one_total)
+
+# So far, we’ve covered how to assign variables values directly in a Python file. However, we often want a user of a program to enter new information into the program.
+
+# How can we do this? As it turns out, another way to assign a value to a variable is through user input.
+
+# While we output a variable’s value using print(), we assign information to a variable using input().
+# The input() function requires a prompt message, which it will print out for the user before they enter the new information.
+# For example:
+
+likes_snakes = input("Do you like snakes? ")
+
+# In the example above, the following would occur:
+
+# The program would print "Do you like snakes? " for the user.
+# The user would enter an answer (e.g., "Yes! I have seven pythons as pets!") and press enter.
+# The variable likes_snakes would be assigned a value of the user’s answer.
+
+# Not only can input() be used for collecting all sorts of different information from a user, but once you have that information stored as a variable you can use it to simulate interaction:
+
+favorite_fruit = input("What is your favorite fruit? ")
+# What is your favorite fruit? mango
+
+print("Oh cool! I like " + favorite_fruit +
+      " too, but I think my favorite fruit is apple.")
+# Oh yeah! I like mango too, but I think my favorite fruit is apple.
