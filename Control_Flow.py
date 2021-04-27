db2cdb08c7e98ab357fa1817cee6378f85757880
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-04-27 13:44:36 -0500

    Magic 8 Ball mini project

--- Control_Flow.py
+++ Control_Flow.py
@@ -4,6 +4,7 @@
 # In order to build control flow into our program, we want to be able to check if something is true or not. A boolean expression is a statement that can either be True or False.
 
 # "Dogs are mammals"
+import random
 statement_one = "Yes"
 
 # My dog is named Pavel.
@@ -114,3 +115,36 @@ elif grade >= 60:
     print("D")
 else:
     print("F")
+
+
+name = "Luis"
+question = "Will I be a good programmer?"
+answer = ""
+
+random_number = random.randint(1, 9)
+
+# print(random_number)
+
+if random_number == 1:
+    answer = "Yes - definitely."
+elif random_number == 2:
+    answer = "It is decidedly so."
+elif random_number == 3:
+    answer = "Without a doubt."
+elif random_number == 4:
+    answer = "Reply hazy, try again."
+elif random_number == 5:
+    answer = "Ask again later."
+elif random_number == 6:
+    answer = "IBetter not tell you now."
+elif random_number == 7:
+    answer = "My sources say no."
+elif random_number == 8:
+    answer = "Outlook not so good."
+elif random_number == 9:
+    answer = "Very doubtful."
+else:
+    answer = "Error."
+
+print(name + " " + "asks: " + "question")
+print("Magic 8-Ball's answer: " + answer)
