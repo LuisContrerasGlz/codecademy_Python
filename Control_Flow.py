a794ad7d3d62351d356f27ec6b27b3c33e952ec9
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-04-26 22:36:18 -0500

    progress on Control flow

---
+++ Control_Flow.py
@@ -0,0 +1,30 @@
+# Learn how to build control flow into your python code by including if, else, and elif statements as well as try and except statements.
+# Expect to learn all you need to know about boolean variables and logical operators.
+
+# In order to build control flow into our program, we want to be able to check if something is true or not. A boolean expression is a statement that can either be True or False.
+
+# "Dogs are mammals"
+statement_one = "Yes"
+
+# My dog is named Pavel.
+statement_two = "Yes"
+
+# Dogs make the best pets.
+statement_three = "No"
+
+# Cats are female dogs.
+statement_four = "Yes"
+
+# Relational operators compare two items and return either True or False.
+# For this reason, you will sometimes hear them called comparators.
+# The two relational operators we’ll cover first are:
+
+# Equals: ==
+# Not equals: !=
+# These operators compare two items and return True or False if they are equal or not.
+
+statement_one = (5 * 2) - 1 == 8 + 1
+
+statement_two = 13 - 6 != (3 * 2) + 1
+
+statement_three = 3 * (2 - 1) == 4 - 1
