6c00d873406e600e1bde10e04784235ae7dae69c
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-04-29 18:12:02 -0500

    intro to lists

---
+++ Lists.py
@@ -0,0 +1,8 @@
+# What is a List?
+# In programming, it is common to want to work with collections of data.
+# In Python, a list is one of the many built-in data structures that allows us to work with a collection of data in sequential order.
+
+heights = [61, 70, 67, 64]
+
+ints_and_strings = [1, 2, 3, "four", "five", "Cool"]
+sam_height_and_testscore = ["Sam", 67, 85.5,  True]
