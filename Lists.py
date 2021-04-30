397c949417601021d38648f73bba8b5e4e68a377
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-04-30 14:58:14 -0500

    progress on Lists

--- Lists.py
+++ Lists.py
@@ -60,3 +60,20 @@ orders.append("tulips")
 orders.append("roses")
 
 print(orders)
+
+# When we want to add multiple items to a list, we can use + to combine two lists (this is also known as concatenation).
+# We can only use + with other lists.
+# If we want to add a single element using +, we have to put it into a list with brackets ([])
+
+my_list = [1, 2, 3]
+my_list + [4]
+
+orders = ["daisy", "buttercup", "snapdragon", "gardenia", "lily"]
+
+# Create new orders here:
+new_orders = ["lilac", "iris"]
+
+orders_combined = orders + new_orders
+
+
+broken_prices = [5, 3, 4, 5, 4] + [4]
