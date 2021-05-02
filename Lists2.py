156c8a01e04b7265179a8c289648b8f4e3e17c3e
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-01 23:30:19 -0500

    Range

--- Lists2.py
+++ Lists2.py
@@ -55,3 +55,31 @@ data_science_topics.pop()
 
 data_science_topics.pop(3)
 print(data_science_topics)
+
+# Often, we want to create a list of consecutive numbers in our programs.
+# Suppose we want a list containing the numbers 0 through 9:
+
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Python gives us an easy way of creating these types of lists using a built-in function called range().
+# The function range() takes a single input, and generates numbers starting at 0 and ending at the number before the input.
+# So, if we want the numbers from 0 through 9, we use range(10) because 10 is 1 greater than 9:
+
+my_range = range(10)
+print(my_range)  # Would output: range(0, 10)
+
+# The range() function is unique in that it creates a range object.
+# It is not a typical list like the ones we have been working with.
+# In order to use this object as a list, we have to first convert it using another built-in function called list().
+# The list() function takes in a single input for the object you want to convert.
+# We use the list() function on our range object like this:
+
+print(list(my_range))  # Would output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Your code below:
+
+number_list = range(9)
+print(list(number_list))
+
+zero_to_seven = range(8)
+print(list(zero_to_seven))
