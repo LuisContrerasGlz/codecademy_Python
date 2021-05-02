44c8800544ed3d0424c4cb076b64c54423318e55
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-01 23:17:28 -0500

    insert on Lists

--- Lists2.py
+++ Lists2.py
@@ -6,3 +6,23 @@
 # Select portions of a list (called slicing).
 # Count the number of times that an element appears in a list.
 # Sort a list of items.
+
+# The Python list method .insert() allows us to add an element to a specific index in a list.
+# The .insert() method takes in two inputs:
+
+# The index you want to insert into.
+# The element you want to insert at the specified index.
+# The .insert() method will handle shifting over elements and can be used with negative indices.
+
+store_line = ["Karla", "Maxium", "Martim", "Isabella"]
+store_line.insert(2, "Vikor")
+# Would output:  ['Karla', 'Maxium', 'Vikor', 'Martim', 'Isabella']
+print(store_line)
+
+front_display_list = ["Mango", "Filet Mignon", "Chocolate Milk"]
+print(front_display_list)
+
+# Your code below:
+
+front_display_list.insert(0, "Pineapple")
+print(front_display_list)
