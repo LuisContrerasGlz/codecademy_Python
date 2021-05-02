759a56474e898dd3ce5c62d6cf8603b625bc7de3
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-01 22:26:42 -0500

    progress on Lists

---
+++ Lists2.py
@@ -0,0 +1,8 @@
+# We will start to explore additional ways of working with lists like:
+
+# Add and remove items from a list using a specific index.
+# Create lists with continuous values.
+# Get the length of a list.
+# Select portions of a list (called slicing).
+# Count the number of times that an element appears in a list.
+# Sort a list of items.
