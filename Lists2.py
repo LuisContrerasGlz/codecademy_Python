8f69af1fbf821dc9ff0fd627715cd4d378516dee
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-01 23:24:44 -0500

    remove on Lists

--- Lists2.py
+++ Lists2.py
@@ -26,3 +26,32 @@ print(front_display_list)
 
 front_display_list.insert(0, "Pineapple")
 print(front_display_list)
+
+# Python gives us a method to remove elements at a specific index using a method called .pop().
+# The .pop() method takes an optional single input: The index for the element you want to remove.
+
+cs_topics = ["Python", "Data Structures",
+             "Balloon Making", "Algorithms", "Clowns 101"]
+removed_element = cs_topics.pop()
+
+# Would output: ['Python', 'Data Structures', 'Balloon Making', 'Algorithms']
+# The method can be called without a specific index. Using .pop() without an index will remove whatever the last element of the list is.
+# .pop() is unique in that it will return the value that was removed. If we wanted to know what element was deleted, simply assign a variable to the call of the .pop() method.
+
+cs_topics.pop(2)
+
+# Would output: ['Python', 'Data Structures', 'Algorithms']
+# The method can be called with an optional specific index to remove. In our case, the index 2 removes the value of "Balloon Making".
+# We don’t have to save the removed value to any variable if we don’t care to use it later.
+# Note: Passing in an index that does not exist or calling .pop() on an empty list will both result in an IndexError.
+
+data_science_topics = ["Machine Learning", "SQL",
+                       "Pandas", "Algorithms", "Statistics", "Python 3"]
+
+
+# Your code below:
+data_science_topics.pop()
+
+
+data_science_topics.pop(3)
+print(data_science_topics)
