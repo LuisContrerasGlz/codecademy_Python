044bee40cb76bd5a4665e1acfdfc16796ce1da58
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-07 16:46:06 -0500

    for loop 1

--- Loops.py
+++ Loops.py
@@ -28,3 +28,53 @@ print(ingredients[4])
 # That’s still manageable, We’re writing 5 print() statements (or copying and pasting a few times).
 # Now imagine if we come back to this program and our list had 10, or 24601, or … 100,000,000 elements?
 # It would take an extremely long time and by the end, we could still end up with inconsistencies and mistakes.
+
+# Let’s start with your first type of loop, a for loop, a type of definite iteration.
+# In a for loop, we will know in advance how many times the loop will need to iterate because we will be working on a collection with a predefined length.
+# With for loops, on each iteration, we will be able to perform an action on each element of the collection.
+
+# for <temporary variable> in <collection>:
+#     <action>
+
+# A for keyword indicates the start of a for loop.
+# A <temporary variable> that is used to represent the value of the element in the collection the loop is currently on.
+# An in keyword separates the temporary variable from the collection used for iteration.
+# A <collection> to loop over.
+# An <action> to do anything on each iteration of the loop.
+
+ingredients = ["milk", "sugar", "vanilla extract", "dough", "chocolate"]
+
+for ingredient in ingredients:
+    print(ingredient)
+
+# ingredient is the <temporary variable>.
+# ingredients is our <collection>.
+# print(ingredient) was the <action> performed on every iteration using the temporary variable of ingredient.
+# A temporary variable’s name is arbitrary and does not need to be defined beforehand.
+
+for i in ingredients:
+    print(i)
+
+for item in ingredients:
+    print(item)
+
+# Notice that in all of these examples the print statement is indented.
+# Everything at the same level of indentation after the for loop declaration is included in the loop body and is run on every iteration of the loop.
+
+for ingredient in ingredients:
+    # Any code at this level of indentation
+    # will run on each iteration of the loop
+    print(ingredient)
+
+# If we ever forget to indent, we’ll get an IndentationError or unexpected behavior.
+
+board_games = ["Settlers of Catan", "Carcassone",
+               "Power Grid", "Agricola", "Scrabble"]
+
+sport_games = ["football", "hockey", "baseball", "cricket"]
+
+for game in board_games:
+    print(game)
+
+for sportg in sport_games:
+    print(sportg)
