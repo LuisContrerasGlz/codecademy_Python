897c7923af9cccb14b943c0aef7c0e12e5065634
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-09 22:06:03 -0500

    break from loop

--- Loops.py
+++ Loops.py
@@ -214,3 +214,34 @@ while index < length:
 
 # for number in my_favorite_numbers:
 #   my_favorite_numbers.append(1)
+
+items_on_sale = ["blue shirt", "striped socks",
+                 "knit dress", "red headband", "dinosaur onesie"]
+
+# What does our loop look like if we want to search for the value of "knit dress" and print out "Found it" if it did exist?
+
+for item in items_on_sale:
+    if item == "knit dress":
+        print("Found it")
+
+items_on_sale = ["blue shirt", "striped socks",
+                 "knit dress", "red headband", "dinosaur onesie"]
+
+print("Checking the sale list!")
+
+for item in items_on_sale:
+    print(item)
+    if item == "knit dress":
+        break
+
+print("End of search!")
+
+dog_breeds_available_for_adoption = [
+    "french_bulldog", "dalmatian", "shihtzu", "poodle", "collie"]
+dog_breed_I_want = "dalmatian"
+
+for dog_breed in dog_breeds_available_for_adoption:
+    print(dog_breed)
+    if dog_breed == dog_breed_I_want:
+        print("They have the dog I want!")
+        break
