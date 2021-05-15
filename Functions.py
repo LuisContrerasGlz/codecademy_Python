a5f26bae8ec3aebcfb26959346cc974f8ba771cc
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-15 02:43:54 -0500

    Functions

--- Functions.py
+++ Functions.py
@@ -340,3 +340,24 @@ most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
 print(most_popular1)
 print(most_popular2)
 print(most_popular3)
+
+
+def trip_planner_welcome(name):
+    print("Welcome to tripplanner v1.0 " + name)
+
+
+def destination_setup(origin, destination, estimated_time, mode_of_transport="Car"):
+    print("Your trip starts off in " + origin)
+    print("And you are traveling to " + destination)
+    print("You will be traveling by " + mode_of_transport)
+    print("It will take approximately " + str(estimated_time) + " hours")
+
+
+def estimated_time_rounded(estimated_time):
+    rounded_time = round(estimated_time)
+    return rounded_time
+
+
+# trip_planner_welcome(" <YOUR NAME HERE> ")
+# estimate = estimated_time_rounded(2.43)
+# destination_setup(" <PICK A ORIGIN> ", "<PICK A DESTINATION > ", estimate, "Car")
