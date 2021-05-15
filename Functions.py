5a44733086a72d98f207ff7e024850c2d13024cf
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-15 02:41:26 -0500

    Multiple  Returns

--- Functions.py
+++ Functions.py
@@ -307,3 +307,36 @@ def deduct_expense(budget, expense):
 new_budget_after_shirt = deduct_expense(current_budget, shirt_expense)
 
 print_remaining_budget(new_budget_after_shirt)
+
+# Sometimes we may want to return more than one value from a function.
+# We can return several values by separating them with a comma.
+
+weather_data = ['Sunny', 'Sunny', 'Cloudy', 'Raining', 'Snowing']
+
+
+def threeday_weather_report(weather):
+    first_day = " Tomorrow the weather will be " + weather[0]
+    second_day = " The following day it will be " + weather[1]
+    third_day = " Two days from now it will be " + weather[2]
+    return first_day, second_day, third_day
+
+
+monday, tuesday, wednesday = threeday_weather_report(weather_data)
+
+print(monday)
+print(tuesday)
+print(wednesday)
+
+
+def top_tourist_locations_italy():
+    first = "Rome"
+    second = "Venice"
+    third = "Florence"
+    return first, second, third
+
+
+most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
+
+print(most_popular1)
+print(most_popular2)
+print(most_popular3)
