837046b7880b87bd39222e5676a20162305fa12c
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-17 13:10:11 -0500

    Escape

--- Strings.py
+++ Strings.py
@@ -176,3 +176,16 @@ last_name = "Daily"
 # first_name[0] = "R"
 
 fixed_first_name = "R" + first_name[1:]
+
+# Occasionally when working with strings, you’ll find that you want to include characters that already have a special meaning in python.
+# For example let’s say I create the string
+
+# favorite_fruit_conversation = "He said, "blueberries are my favorite!""
+
+# We’ll have accidentally ended the string before we wanted to by including the " character.
+# The way we can do this is by introducing escape characters.
+# By adding a backslash in front of the special character we want to escape, \", we can include it in a string.
+
+favorite_fruit_conversation = "He said, \"blueberries are my favorite!\""
+
+password = "theycallme\"crazy\"91"
