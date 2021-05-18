573e1ec742e13d1e1e486a177656f960eea7e88e
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-18 14:01:09 -0500

    Intro String methods

---
+++ String_Methods.py
@@ -0,0 +1,10 @@
+# Do you have a gigantic string that you need to parse for information?
+# Do you need to sanitize a users input to work in a function?
+# Do you need to be able to generate outputs with variable values?
+# All of these things can be accomplished with string methods!
+
+# Python comes with built-in string methods that gives you the power to perform complicated tasks on strings very quickly and efficiently.
+# These string methods allow you to change the case of a string, split a string into many smaller strings,
+# join many small strings together into a larger string, and allow you to neatly combine changing variables with string outputs.
+
+# string_name.string_method(arguments) a string method is called at the end of a string and each one has its own method specific arguments.
