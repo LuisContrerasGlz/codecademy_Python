2ddfb6bda9e1e55845c6530bd7e148a7c9f28d13
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-18 14:10:45 -0500

    Splitting Strings

--- String_Methods.py
+++ String_Methods.py
@@ -37,3 +37,17 @@ poem_author_fixed = poem_author.upper()
 
 print(poem_author)
 print(poem_author_fixed)
+
+# .upper(), .lower(), and .title() all are performed on an existing string and produce a string in return.
+# .split() is performed on a string, takes one argument, and returns a list of substrings found between the given argument (which in the case of .split() is known as the delimiter).
+
+# string_name.split(delimiter) If you do not provide an argument for .split() it will default to splitting at spaces.
+
+man_its_a_hot_one = "Like seven inches from the midday sun"
+print(man_its_a_hot_one.split())
+# => ['Like', 'seven', 'inches', 'from', 'the', 'midday', 'sun']
+
+# .split returned a list with each word in the string. Important to note: if we run .split() on a string with no spaces, we will get the same string in return.
+
+line_one = "The sky has given over"
+line_one_words = line_one.split()
