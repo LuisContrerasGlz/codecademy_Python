9c0478f016ea629598050a3f7565e222cadf9cbe
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-19 02:42:16 -0500

    find

--- String_Methods.py
+++ String_Methods.py
@@ -208,3 +208,19 @@ Nathan Pinchback Tomer, who adopted the name Jean Tomer early in his literary ca
 """
 
 toomer_bio_fixed = toomer_bio.replace("Tomer", "Toomer")
+
+# Another interesting string method is .find().
+# .find() takes a string as an argument and searching the string it was run on for that string.
+# It then returns the first index value where that string is located.
+
+print('smooth'.find('t'))
+# => '4'
+
+# You can also search for larger strings, and .find() will return the index value of the first character of that string.
+
+print("smooth".find('oo'))
+# => '2'
+
+god_wills_it_line_one = "The very earth will disown you"
+
+disown_placement = god_wills_it_line_one.find("disown")
