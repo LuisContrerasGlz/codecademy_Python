3646b34a68d78cb04dec87206c45dae631118c1f
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-19 02:23:22 -0500

    Joining Strings 2

--- String_Methods.py
+++ String_Methods.py
@@ -131,3 +131,32 @@ reapers_line_one_words = ["Black", "reapers", "with",
                           "the", "sound", "of", "steel", "on", "stones"]
 
 reapers_line_one = " ".join(reapers_line_one_words)
+
+# In fact, you can use any string as a delimiter to join together a list of strings.
+
+santana_songs = ['Oye Como Va', 'Smooth',
+                 'Black Magic Woman', 'Samba Pa Ti', 'Maria Maria']
+
+# One often used string is a comma , because then we can create a string of comma separated variables, or CSV.
+
+santana_songs_csv = ','.join(santana_songs)
+print(santana_songs_csv)
+# => 'Oye Como Va,Smooth,Black Magic Woman,Samba Pa Ti,Maria Maria'
+
+# You’ll often find data stored in CSVs because it is an efficient, simple file type used by popular programs like Excel or Google Spreadsheets.
+
+# You can also join using escape sequences as the delimiter.
+
+smooth_fifth_verse_lines = ['Well I\'m from the barrio', 'You hear my rhythm on your radio',
+                            'You feel the turning of the world so soft and slow', 'Turning you \'round and \'round']
+
+smooth_fifth_verse = '\n'.join(smooth_fifth_verse_lines)
+
+print(smooth_fifth_verse)
+
+winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among',
+                      'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
+
+winter_trees_full = '\n'.join(winter_trees_lines)
+
+print(winter_trees_full)
