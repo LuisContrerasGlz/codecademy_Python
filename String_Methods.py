06c6da92563e476ab9344b5a5ed65996cc35cae5
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-19 03:28:44 -0500

    format 2

--- String_Methods.py
+++ String_Methods.py
@@ -246,3 +246,31 @@ print(favorite_song_statement("Smooth", "Santana"))
 def poem_title_card(title, poet):
     poem_desc = "The poem \"{}\" is written by {}.".format(title, poet)
     return poem_desc
+
+# .format() can be made even more legible for other people reading your code by including keywords.
+
+
+def favorite_song_statement(song, artist):
+    return "My favorite song is {song} by {artist}.".format(song=song, artist=artist)
+
+# Now it is clear to anyone reading the string what it supposed to return,
+# they don’t even need to look at the arguments of .format() in order to get a clear understanding of what is supposed to happen.
+# You can even reverse the order of artist and song in the code above and it will work the same way.
+# This makes writing AND reading the code much easier.
+
+
+def poem_description(publishing_date, author, title, original_work):
+    poem_desc = "The poem {title} by {author} was originally published in {original_work} in {publishing_date}.".format(
+        publishing_date=publishing_date, author=author, title=title, original_work=original_work)
+    return poem_desc
+
+
+author = "Shel Silverstein"
+title = "My Beard"
+original_work = "Where the Sidewalk Ends"
+publishing_date = "1974"
+
+my_beard_description = poem_description(
+    publishing_date, author, title, original_work)
+
+print(my_beard_description)
