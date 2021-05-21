59180e6e97fb588fe9cc784c8bca866a3344511e
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-20 20:44:43 -0500

    Module namespace

--- Modules.py
+++ Modules.py
@@ -12,6 +12,8 @@
 # One common library that comes as part of the Python Standard Library is datetime.
 # datetime helps you work with dates and times in Python.
 
+from matplotlib import pyplot as plt
+import codecademylib3_seaborn
 import random
 from datetime import datetime
 
@@ -38,3 +40,28 @@ randomer_number = random.choice(random_list)
 
 # Print randomer_number:
 print(randomer_number)
+
+# Notice that when we want to invoke the randint() function we call random.randint().
+# This is default behavior where Python offers a namespace for the module.
+# A namespace isolates the functions, classes, and variables defined in the module from the code in the file doing the importing.
+# Your local namespace, meanwhile, is where your code is run.
+
+# Python defaults to naming the namespace after the module being imported, but sometimes this name could be ambiguous or lengthy.
+# Sometimes, the module’s name could also conflict with an object you have defined within your local namespace.
+# Fortunately, this name can be altered by aliasing using the as keyword:
+# import module_name as name_you_pick_for_the_module
+# Aliasing is most often done if the name of the library is long and typing the full name every time you want to use one of its functions is laborious.
+
+# You might also occasionally encounter import *.
+# The * is known as a “wildcard” and matches anything and everything.
+# This syntax is considered dangerous because it could pollute our local namespace.
+# Pollution occurs when the same name could apply to two possible things.
+
+
+numbers_a = range(1, 13)
+
+numbers_b = random.sample(range(1000), 12)
+
+plt.plot(numbers_a, numbers_b)
+
+plt.show()
