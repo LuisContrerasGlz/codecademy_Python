ffcccac5764abb655c4d460397c135d89fdf305c
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-20 20:58:10 -0500

    Modules Python Files and Scope

--- Modules.py
+++ Modules.py
@@ -91,3 +91,14 @@ print(two_decimal_points)
 
 four_decimal_points = Decimal('0.53') * Decimal('0.65')
 print(four_decimal_points)
+
+# Even files inside the same directory do not have access to each other’s variables, functions, classes, or any other code.
+# So if I have a file sandwiches.py and another file hungry_people.py, how do I give my hungry people access to all the sandwiches I defined?
+# Well, files are actually modules, so you can give a file access to another file’s content using that glorious import statement.
+# With a single line of from sandwiches import sandwiches at the top of hungry_people.py, the hungry people will have all the sandwiches they could ever want.
+
+# Import library:
+# from library import always_three
+
+# Call function:
+# always_three()
