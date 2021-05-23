38f22c99346041edba27c903d7e357f478ff8624
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-22 22:34:50 -0500

    Add A Key

--- Dictionaries.py
+++ Dictionaries.py
@@ -56,3 +56,21 @@ children = {"von Trapp": ["Johannes", "Rosmarie",
 empty_dict = {}
 
 my_empty_dictionary = {}
+
+# To add a single key: value pair to a dictionary, we can use the syntax:
+# dictionary[key] = value
+
+menu = {"oatmeal": 3, "avocado toast": 6,
+        "carrot juice": 5, "blueberry muffin": 2}
+
+# Add a new key:
+
+menu["cheesecake"] = 8
+
+# Now, menu looks like: {"oatmeal": 3, "avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2, "cheesecake": 8}
+
+animals_in_zoo = {}
+animals_in_zoo['zebras'] = 8
+animals_in_zoo['monkeys'] = 12
+animals_in_zoo['dinosaurs'] = 0
+print(animals_in_zoo)
