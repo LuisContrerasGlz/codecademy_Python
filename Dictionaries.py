6899b7422ff7d3eda089a8fe13ea365047dc13a5
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-23 10:45:33 -0500

    Add Multiple Keys

--- Dictionaries.py
+++ Dictionaries.py
@@ -74,3 +74,17 @@ animals_in_zoo['zebras'] = 8
 animals_in_zoo['monkeys'] = 12
 animals_in_zoo['dinosaurs'] = 0
 print(animals_in_zoo)
+
+# If we wanted to add multiple key : value pairs to a dictionary at once, we can use the .update() method.
+sensors = {"living room": 21, "kitchen": 23, "bedroom": 20}
+
+# dd all three items to the sensors dictionary.
+sensors.update({"pantry": 22, "guest room": 25, "patio": 34})
+
+# Now, sensors looks like:
+# {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22, "guest room": 25, "patio": 34}
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+print(user_ids)
