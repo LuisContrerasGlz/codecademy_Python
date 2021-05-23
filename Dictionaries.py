936878710697a739a4739ceeb344b916e8101fb9
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-22 20:06:11 -0500

    Invalid Keys

--- Dictionaries.py
+++ Dictionaries.py
@@ -38,3 +38,15 @@ person = {"name": "Shuri", "age": 18, "family": ["T'Chaka", "Ramonda"]}
 
 translations = {"mountain": "orod", "bread": "bass",
                 "friend": "mellon", "horse": "roch"}
+
+# We can have a list or a dictionary as a value of an item in a dictionary, but we cannot use these data types as keys of the dictionary.
+# If we try to, we will get a TypeError.
+
+# powers = {[1, 2, 4, 8, 16]: 2, [1, 3, 9, 27, 81]: 3} will get TypeError: unhashable type: 'list'
+# The word “unhashable” in this context means that this ‘list’ is an object that can be changed.
+# Dictionaries in Python rely on each key having a hash value, a specific identifier for the key.
+# If the key can change, that hash value would not be reliable.
+# So the keys must always be unchangeable, hashable data types, like numbers or strings.
+
+children = {"von Trapp": ["Johannes", "Rosmarie",
+                          "Eleonore"], "Corleone": ["Sonny", "Fredo", "Michael"]}
