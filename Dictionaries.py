7a4d074a65adc5e0997424971401bf5a15a09fa4
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-24 10:21:47 -0500

    Get an Invalid Key

--- Dictionaries.py
+++ Dictionaries.py
@@ -160,3 +160,25 @@ zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "
 
 print(zodiac_elements['earth'])
 print(zodiac_elements['fire'])
+
+building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601,
+                    "Ping An": 599, "Lotte World Tower": 554.5, "One World Trade": 541.3}
+
+print(building_heights["Landmark 81"])
+
+# "Landmark 81" does not exist as a key in the building_heights dictionary! So this will throw a KeyError:
+# KeyError: 'Landmark 81'
+
+# One way to avoid this error is to first check if the key exists in the dictionary:
+
+key_to_check = "Landmark 81"
+
+if key_to_check in building_heights:
+    print(building_heights["Landmark 81"])
+
+# This will not throw an error, because key_to_check in building_heights will return False, and so we never try to access the key.
+
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": [
+    "Taurus", "Virgo", "Capricorn"], "air": ["Gemini", "Libra", "Aquarius"], "energy": "Not a Zodiac element"}
+
+print(zodiac_elements["energy"])
