625664f0847ac8a5284aab6472619a3cac61e22e
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-23 22:26:24 -0500

    Overwrite Values

--- Dictionaries.py
+++ Dictionaries.py
@@ -88,3 +88,23 @@ user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
 
 user_ids.update({"theLooper": 138475, "stringQueen": 85739})
 print(user_ids)
+
+# We know that we can add a key by using syntax like:
+menu["avocado toast"] = 7
+
+# This will create a key "avocado toast" and set the value to 7.
+# But what if we already have an 'avocado toast' entry in the menu dictionary?
+# In that case, our value assignment would overwrite the existing value attached to the key 'avocado toast'.
+
+menu = {"oatmeal": 3, "avocado toast": 6,
+        "carrot juice": 5, "blueberry muffin": 2}
+menu["oatmeal"] = 5
+print(menu)
+# Output: {"oatmeal": 5, "avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2}
+
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck",
+                 "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+
+oscar_winners["Supporting Actress"] = "Viola Davis"
+
+oscar_winners["Best Picture"] = "Moonlight"
