e625c1b5b351c7484c9e924e9f2541e658aff7c8
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-25 19:53:35 -0500

    Safely Get a Key

--- Dictionaries.py
+++ Dictionaries.py
@@ -202,3 +202,37 @@ try:
     print(caffeine_level["matcha"])
 except KeyError:
     print("Unknown Caffeine Level")
+
+# We saw in that we had to add a key:value pair to a dictionary in order to avoid a KeyError.
+# This solution is not sustainable.
+# We can’t predict every key a user may call and add all of those placeholder values to our dictionary!
+# Dictionaries have a .get() method to search for a value instead of the my_dict[key] notation we have been using.
+# If the key you are trying to .get() does not exist, it will return None by default:
+
+building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601,
+                    "Ping An": 599, "Lotte World Tower": 554.5, "One World Trade": 541.3}
+
+# this line will return 632:
+building_heights.get("Shanghai Tower")
+
+# this line will return None:
+building_heights.get("My House")
+
+# You can also specify a value to return if the key doesn’t exist.
+# For example, we might want to return a building height of 0 if our desired building is not in the dictionary:
+
+building_heights.get('Shanghai Tower', 0)
+# 632
+building_heights.get('Mt Olympus', 0)
+# 0
+building_heights.get('Kilimanjaro', 'No Value')
+# 'No Value'
+
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921,
+            "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+
+tc_id = user_ids.get("teraCoder", 100000)
+stack_id = user_ids.get("superStackSmash", 100000)
+
+print(tc_id)
+print(stack_id)
