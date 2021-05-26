6257655973acaa3086cec6fdcfe16d012cacd3ae
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-25 19:49:06 -0500

    Try/Except to Get a Key

--- Dictionaries.py
+++ Dictionaries.py
@@ -182,3 +182,23 @@ zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "
     "Taurus", "Virgo", "Capricorn"], "air": ["Gemini", "Libra", "Aquarius"], "energy": "Not a Zodiac element"}
 
 print(zodiac_elements["energy"])
+
+
+# We saw that we can avoid KeyErrors by checking if a key is in a dictionary first.
+# Another method we could use is a try/except:
+
+key_to_check = "Landmark 81"
+try:
+    print(building_heights[key_to_check])
+except KeyError:
+    print("That key doesn't exist!")
+
+# When we try to access a key that doesn’t exist, the program will go into the except block and print "That key doesn't exist!".
+
+caffeine_level = {"espresso": 64, "chai": 40, "decaf": 0, "drip": 120}
+caffeine_level["matcha"] = 30
+
+try:
+    print(caffeine_level["matcha"])
+except KeyError:
+    print("Unknown Caffeine Level")
