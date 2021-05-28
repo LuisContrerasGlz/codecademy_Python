86ad9a22e37c3d6dc8c250f198b1a1cad6089911
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-28 12:41:25 -0500

    Reading a CSV File

--- Files.py
+++ Files.py
@@ -10,6 +10,7 @@
 # We could read that file like this:
 # script.py
 
+import csv
 with open('real_cool_document.txt') as cool_doc:
     cool_contents = cool_doc.read()
 print(cool_contents)
@@ -153,3 +154,30 @@ with open('fun_file.txt') as close_this_file:
 
 with open('logger.csv') as log_csv_file:
     print(log_csv_file.read())
+
+# Even though we can read these lines as text without a problem, there are ways to access the data in a format better suited for programming purposes.
+# In Python we can convert that data into a dictionary using the csv library’s DictReader object.
+
+
+list_of_email_addresses = []
+with open('users.csv', newline='') as users_csv:
+    user_reader = csv.DictReader(users_csv)
+    for row in user_reader:
+        list_of_email_addresses.append(row['Email'])
+
+# In the above code we first import our csv library, which gives us the tools to parse our CSV file.
+# We then create the empty list list_of_email_addresses which we’ll later populate with the email addresses from our CSV.
+# Then we open the users.csv file with the temporary variable users_csv.
+# We pass the additional keyword argument newline='' to the file opening open() function so that we don’t accidentally mistake a line break in one of our data fields as a new row in our CSV
+
+# After opening our new CSV file we use csv.DictReader(users_csv) which converts the lines of our CSV file to Python dictionaries which we can use access methods for.
+# The keys of the dictionary are, by default, the entries in the first line of our CSV file.
+# Since our CSV’s first line calls the third field in our CSV “Email“, we can use that as the key in each row of our DictReader.
+# When we iterate through the rows of our user_reader object, we access all of the rows in our CSV as dictionaries (except for the first row, which we used to label the keys of our dictionary).
+# By accessing the 'Email' key of each of these rows we can grab the email address in that row and append it to our list_of_email_addresses.
+
+
+with open('cool_csv.csv') as cool_csv_file:
+    cool_csv_dict = csv.DictReader(cool_csv_file)
+    for row in cool_csv_dict:
+        print(row['Cool Fact'])
