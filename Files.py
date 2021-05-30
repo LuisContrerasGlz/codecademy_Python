56b14116bc58cea397d5f09eb595e5c756985947
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-30 01:16:47 -0500

    Reading a JSON File

--- Files.py
+++ Files.py
@@ -10,6 +10,7 @@
 # We could read that file like this:
 # script.py
 
+import json
 import csv
 with open('real_cool_document.txt') as cool_doc:
     cool_contents = cool_doc.read()
@@ -234,3 +235,40 @@ with open('logger.csv', 'w') as logger_csv:
     log_writer.writeheader()
     for line in access_log:
         log_writer.writerow(line)
+
+# CSV isn’t the only file format that Python has a built-in library for.
+# We can also use Python’s file tools to read and write JSON.
+
+# JSON, an abbreviation of JavaScript Object Notation, is a file format inspired by the programming language JavaScript.
+# The name, like CSV is a bit of a misnomer — some JSON is not valid JavaScript (and plenty of JavaScript is not valid JSON).
+# JSON’s format is endearingly similar to Python dictionary syntax, and so JSON files might be easy to read from a Python developer standpoint.
+# Nonetheless, Python comes with a json package that will help us parse JSON files into actual Python dictionaries.
+
+# purchase_14781239.json
+# {
+# 'user': 'ellen_greg',
+# 'action': 'purchase',
+# 'item_id': '14781239',
+# }
+
+# We would be able to read that in as a Python dictionary with the following code:
+# json_reader.py
+
+
+with open('purchase_14781239.json') as purchase_json:
+    purchase_data = json.load(purchase_json)
+
+print(purchase_data['user'])
+# Prints 'ellen_greg'
+
+# First we import the json package.
+# We opened the file using our trusty open() command.
+# Since we’re opening it in read-mode we just need to pass the file name. We save the file in the temporary variable purchase_json.
+# We continue by parsing purchase_json using json.load(), creating a Python dictionary out of the file.
+# Saving the results into purchase_data means we can interact with it.
+# We print out one of the values of the JSON file by keying into the purchase_data object.
+
+
+with open('message.json') as message_json:
+    message = json.load(message_json)
+    print(message['text'])
