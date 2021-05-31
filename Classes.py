deb5e0a22d432e0a35c82f65f29a0cabe21cd71e
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-05-30 20:03:06 -0500

    Instantiation

--- Classes.py
+++ Classes.py
@@ -38,3 +38,22 @@ class CoolClass:
 
 class Facade:
     pass
+
+# A class doesn’t accomplish anything simply by being defined.
+# A class must be instantiated.
+# In other words, we must create an instance of the class, in order to breathe life into the schematic.
+# Instantiating a class looks a lot like calling a function.
+
+
+# We would be able to create an instance of our defined CoolClass as follows:
+cool_instance = CoolClass()
+
+# Above, we created an object by adding parentheses to the name of the class.
+# We then assigned that new instance to the variable cool_instance for safe-keeping so we can access our instance of CoolClass at a later time.
+
+
+class Facade:
+    pass
+
+
+facade_1 = Facade()
