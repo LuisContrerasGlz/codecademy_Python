011bf15e176b5b1491a5f13f3960037583f5d3a5
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-06-01 13:36:06 -0500

    Super

--- Inheritance_and_Polymorphism.py
+++ Inheritance_and_Polymorphism.py
@@ -157,3 +157,45 @@ class User:
 class Admin(User):
     def edit_message(self, message, new_text):
         message.text = new_text
+
+# Overriding methods is really useful in some cases but sometimes we want to add some extra logic to the existing method.
+# In order to do that we need a way to call the method from the parent class.
+# Python gives us a way to do that using super().
+# super() gives us a proxy object.
+# With this proxy object, we can invoke the method of an object’s parent class (also called its superclass).
+
+# We call the required function as a method on super():
+
+
+class Sink:
+    def __init__(self, basin, nozzle):
+        self.basin = basin
+        self.nozzle = nozzle
+
+
+class KitchenSink(Sink):
+    def __init__(self, basin, nozzle, trash_compactor=None):
+        super().__init__(basin, nozzle)
+        if trash_compactor:
+            self.trash_compactor = trash_compactor
+
+# Above we defined two classes.
+# First, we defined a Sink class with a constructor that assigns a rinse basin and a sink nozzle to a Sink instance.
+# Afterwards, we defined a KitchenSink class that inherits from Sink.
+# KitchenSink‘s constructor takes an additional parameter, a trash_compactor.
+# KitchenSink then calls the constructor for Sink with the basin and nozzle it received using the super() function, with this line of code:
+# super().__init__(basin, nozzle)
+# This line says: “call the constructor (the function called __init__()) of the class that is this class’s parent class.”
+
+
+class PotatoSalad:
+    def __init__(self, potatoes, celery, onions):
+        self.potatoes = potatoes
+        self.celery = celery
+        self.onions = onions
+
+
+class SpecialPotatoSalad(PotatoSalad):
+    def __init__(self, potatoes, celery, onions):
+        super().__init__(potatoes, celery, onions)
+        self.raisins = 40
