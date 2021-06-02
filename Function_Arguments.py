d7649aa94027b2d8c3ad55a2b30702d49dd078fb
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-06-02 00:53:38 -0500

    Using Keyword and Positional Arguments

--- Function_Arguments.py
+++ Function_Arguments.py
@@ -8,6 +8,8 @@
 # And function calls are the places a function is invoked, with parentheses, after its definition
 
 # The "def" keyword is the start of a function definition
+import reqs as requests
+from bs4 import BeautifulSoup
 import os
 from review_lib import get_next_review, submit_review
 from record_library import place_record, rotate_record, drop_needle
@@ -206,3 +208,30 @@ def make_folders(folders_list, nest=False):
 
 
 make_folders(['Music', 'fun', 'parliament'])
+
+# Not all of your arguments need to have default values.
+# But Python will only accept functions defined with their parameters in a specific order.
+# The required parameters need to occur before any parameters with a default argument.
+
+# Raises a TypeError
+
+
+def create_user(is_admin=False, username, password):
+    create_email(username, password)
+    set_permissions(is_admin)
+
+# In the above code, we attempt to define a default argument for is_admin without defining default arguments for the later parameters: username and password.
+# If we want to give is_admin a default argument, we need to list it last in our function definition:
+
+# Works perfectly
+
+
+def create_user(username, password, is_admin=False):
+    create_email(username, password)
+    set_permissions(is_admin)
+
+
+def get_id(html_id, website="http://coolsite.com"):
+    request = requests.get(website)
+    parsed_html = BeautifulSoup(website.content, features="html.parser")
+    return parsed_html.find(id_=html_id)
