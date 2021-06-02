fbedb1420bfd8c279987c003d07734e14c734aa8
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-06-02 00:58:22 -0500

    Keyword Arguments

--- Function_Arguments.py
+++ Function_Arguments.py
@@ -8,6 +8,7 @@
 # And function calls are the places a function is invoked, with parentheses, after its definition
 
 # The "def" keyword is the start of a function definition
+import shapes
 import reqs as requests
 from bs4 import BeautifulSoup
 import os
@@ -235,3 +236,33 @@ def get_id(html_id, website="http://coolsite.com"):
     request = requests.get(website)
     parsed_html = BeautifulSoup(website.content, features="html.parser")
     return parsed_html.find(id_=html_id)
+
+# When we call a function in Python, we need to list the arguments to that function to match the order of the parameters in the function definition.
+# We don’t necessarily need to do this if we pass keyword arguments.
+# We use keyword arguments by passing arguments to a function with a special syntax that uses the names of the parameters.
+# This is useful if the function has many optional default arguments or if the order of a function’s parameters is hard to tell.
+
+# Define a function with a bunch of default arguments
+
+
+def log_message(logging_style="shout", message="", font="Times", date=None):
+    if logging_style == 'shout':
+        # capitalize the message
+        message = message.upper()
+    print(message, date)
+
+
+# Now call the function with keyword arguments
+log_message(message="Hello from the past", date="November 20, 1693")
+
+
+def draw_shape(shape_name="box", character="x", line_breaks=True):
+    shape = shapes.draw_shape(shape_name, character)
+    if not line_breaks:
+        print(shape[1:-1])
+    else:
+        print(shape)
+
+
+# call draw_shape() with keyword arguments here:
+draw_shape(character='m', line_breaks=False)
