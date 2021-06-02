e36e7333b7261b44c3e1c96674f90461c6412b8c
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-06-02 11:19:21 -0500

    Using Both Keyword and Positional Unpacking

--- Function_Arguments.py
+++ Function_Arguments.py
@@ -475,3 +475,17 @@ create_products(
     Baseball='3',
     Shirt='14',
     Guitar='70')
+
+
+def remove(filename, *args, **kwargs):
+    with open(filename) as file_obj:
+        text = file_obj.read()
+    for arg in args:
+        text = text.replace(arg, "")
+    for kwarg, replacement in kwargs.items():
+        text = text.replace(kwarg, replacement)
+    return text
+
+
+print(remove("text.txt", "generous", "gallant",
+             fond="amused by", Robin="Mr. Robin"))
