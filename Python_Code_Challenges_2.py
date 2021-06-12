4c33fea7b7d5668cb2a4f89f17b8a7d4e7b490eb
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-06-11 19:04:35 -0500

    Python code 5

--- Python_Code_Challenges_2.py
+++ Python_Code_Challenges_2.py
@@ -57,3 +57,12 @@ def in_range(num, lower, upper):
   if(num >= lower and num <= upper):
     return True
   return False
+
+# Create a function named same_name() that has two parameters named your_name and my_name.
+# If our names are identical, return True. Otherwise, return False.
+
+def same_name(your_name, my_name):
+  if (your_name == my_name):
+    return True
+  else:
+    return False
