673935def536eebb96d204b461d19ebef84e61b1
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-06-11 19:08:02 -0500

    Python code 7

--- Python_Code_Challenges_2.py
+++ Python_Code_Challenges_2.py
@@ -77,3 +77,15 @@ def always_false(num):
     return True
   else:
     return False
+
+# Create a function named movie_review() that has one parameter named rating.
+# If rating is less than or equal to 5, return "Avoid at all costs!".
+# If rating is between 5 and 9, return "This one was fun.".
+# If rating is 9 or above, return "Outstanding!"
+
+def movie_review(rating):
+  if(rating <= 5):
+    return "Avoid at all costs!"
+  if(rating < 9):
+    return "This one was fun."
+  return "Outstanding!"
