a5103290025548119860144e67c5717d9b6ad3e5
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-06-12 02:20:04 -0500

    Python code 12

--- Python_Code_Challenges_2.py
+++ Python_Code_Challenges_2.py
@@ -141,3 +141,12 @@ def more_than_n(lst, item, n):
     return True
   else:
     return False
+
+# Write a function named combine_sort that has two parameters named lst1 and lst2.
+# The function should combine these two lists into one new list and sort the result.
+# Return the new sorted list.
+
+def combine_sort(lst1, lst2):
+  unsorted=lst1 + lst2
+  sortedList=sorted(unsorted)
+  return sortedList
