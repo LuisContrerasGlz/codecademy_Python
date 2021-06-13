d96f8e25939c39301ae759dada83d97949368ae5
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-06-13 02:26:14 -0500

    Python code 19

--- Python_Code_Challenges_2.py
+++ Python_Code_Challenges_2.py
@@ -212,3 +212,9 @@ print(square_root(100))
 def tip(total, percentage):
   tip_amount=(total * percentage) / 100
   return tip_amount
+
+# Write a function named introduction() that has two parameters named first_name and last_name.
+# The function should return the last_name, followed by a comma, a space, first_name another space, and finally last_name.
+
+def introduction(first_name, last_name):
+  return last_name + ", " + first_name + " " + last_name
