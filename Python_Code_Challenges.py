c8ac26baaf8d9acadf652dc45b084ff80982d811
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-06-13 03:38:02 -0500

    Python code 28

--- Python_Code_Challenges.py
+++ Python_Code_Challenges.py
@@ -24,3 +24,19 @@ def reverse_string(word):
     for i in range(len(word)-1, -1, -1):
         reverse += word[i]
     return reverse
+
+# Create a function named add_exclamation that has one parameter named word.
+# This function should add exclamation points to the end of word until word is 20 characters long.
+# If word is already at least 20 characters long, just return word.
+
+
+def add_exclamation(word):
+    while(len(word) < 20):
+        word += "!"
+    return word
+
+
+print(add_exclamation("Codecademy"))
+# should print Codecademy!!!!!!!!!!
+print(add_exclamation("Codecademy is the best place to learn"))
+# should print Codecademy is the best place to learn
