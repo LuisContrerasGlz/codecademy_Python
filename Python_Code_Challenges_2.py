994f23428972d441180de3cbfeec7b58c13e2589
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-06-13 03:33:29 -0500

    Python code 24

--- Python_Code_Challenges_2.py
+++ Python_Code_Challenges_2.py
@@ -273,3 +273,18 @@ print(count_char_x("mississippi", "s"))
 # should print 4
 print(count_char_x("mississippi", "m"))
 # should print 1
+
+# Create a function called x_length_words that takes a string named sentence and an integer named x as parameters.
+# This function should return True if every word in sentence has a length greater than or equal to x.
+
+def x_length_words(sentence, x):
+  words=sentence.split(" ")
+  for word in words:
+    if len(word) < x:
+      return False
+  return True
+
+print(x_length_words("i like apples", 2))
+# should print False
+print(x_length_words("he likes apples", 2))
+# should print True
